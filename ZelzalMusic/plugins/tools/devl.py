d6dec59d0df5e7338b22861820e39df7666f28f5
NoName <57182847+ali-now447@users.noreply.github.com>
2024-02-10 18:54:20 +0300

    Update devl.py

--- ZelzalMusic/plugins/tools/devl.py
+++ ZelzalMusic/plugins/tools/devl.py
@@ -1,1 +1,28 @@
+import os
+import random
+import asyncio
+from pyrogram import Client,filters
+from strings.filters import command
+from pyrogram.types import (Message,
+InlineKeyboardMarkup,InlineKeyboardButton)
+from typing import Union
+from ZelzalMusic import app
 
+@app.on_message(
+    command(["المطور"])
+    & filters.group
+)
+async def yas(client, message):
+    usr = await client.get_chat("lllby")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"≭︰Information Devloper ↯.\n\n━─━─────━─────━─━\n\n≭︰Name ↬ ❲{name}❳\n≭︰Bio ↬{usr.bio}", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
